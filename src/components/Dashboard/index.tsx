390c40ddb2eab4b5f8da3fdb007e65725c189409
Mateus <mateushbsb@gmail.com>
2022-02-06 14:21:06 -0300

    summary component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,10 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles";
+
 export function Dashboard() {
   return(
-    <h1>Dashboard</h1>
+    <Container>
+      <Summary />
+    </Container>
   )
 }
--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,1 +1,8 @@
-import {} from 'styled-components';
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2.5rem 1rem;
+
+`;
